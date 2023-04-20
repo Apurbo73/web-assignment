ecfa3b0a2261ac6f7b3e3537b32a035291c875dc
Apurbo73 <apurbodebnath895@gmail.com>
2023-04-20 17:47:50 +0600

    first commit

---
+++ login_validation.js
@@ -0,0 +1,22 @@
+// Log In validation
+function logformvalidation() {
+  var logMail = document.getElementById("logInemail").value;
+  if (logMail.length == 0) {
+    alert("Enter your mail");
+    return false;
+  }
+  var logInpass = document.getElementById("logpass").value;
+  var logPassPattern = /((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%&*<>?+_-])).{8,20}/;
+  if (logInpass.length == 0) {
+    alert("Enter your password");
+    return false;
+  } else if (!logInpass.match(logPassPattern)) {
+    alert(" Incorrect Password");
+    return false;
+  } else {
+    alert("Successfully logged in");
+    window.location = "index.html";
+
+    return false;
+  }
+}
